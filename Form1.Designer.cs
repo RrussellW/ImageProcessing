7d4fb40273cdff577d904cc093d1627569c55404
Renz Russell T. Wacan <rrwacan@gmail.com>
2023-11-28 03:55:12 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace ImageProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnGreyscale = new System.Windows.Forms.Button();
+            this.btnInvert = new System.Windows.Forms.Button();
+            this.btnHistogram = new System.Windows.Forms.Button();
+            this.btnSepia = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnWebcam = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(294, 249);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(494, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(294, 249);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(313, 12);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(175, 23);
+            this.btnCopy.TabIndex = 2;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnGreyscale
+            // 
+            this.btnGreyscale.Location = new System.Drawing.Point(313, 41);
+            this.btnGreyscale.Name = "btnGreyscale";
+            this.btnGreyscale.Size = new System.Drawing.Size(175, 23);
+            this.btnGreyscale.TabIndex = 3;
+            this.btnGreyscale.Text = "Greyscale";
+            this.btnGreyscale.UseVisualStyleBackColor = true;
+            this.btnGreyscale.Click += new System.EventHandler(this.btnGreyscale_Click);
+            // 
+            // btnInvert
+            // 
+            this.btnInvert.Location = new System.Drawing.Point(314, 70);
+            this.btnInvert.Name = "btnInvert";
+            this.btnInvert.Size = new System.Drawing.Size(175, 23);
+            this.btnInvert.TabIndex = 4;
+            this.btnInvert.Text = "Color Inversion";
+            this.btnInvert.UseVisualStyleBackColor = true;
+            this.btnInvert.Click += new System.EventHandler(this.btnInvert_Click);
+            // 
+            // btnHistogram
+            // 
+            this.btnHistogram.Location = new System.Drawing.Point(313, 99);
+            this.btnHistogram.Name = "btnHistogram";
+            this.btnHistogram.Size = new System.Drawing.Size(175, 23);
+            this.btnHistogram.TabIndex = 5;
+            this.btnHistogram.Text = "Histogram";
+            this.btnHistogram.UseVisualStyleBackColor = true;
+            this.btnHistogram.Click += new System.EventHandler(this.btnHistogram_Click);
+            // 
+            // btnSepia
+            // 
+            this.btnSepia.Location = new System.Drawing.Point(313, 128);
+            this.btnSepia.Name = "btnSepia";
+            this.btnSepia.Size = new System.Drawing.Size(175, 23);
+            this.btnSepia.TabIndex = 6;
+            this.btnSepia.Text = "Sepia";
+            this.btnSepia.UseVisualStyleBackColor = true;
+            this.btnSepia.Click += new System.EventHandler(this.btnSepia_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 267);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(175, 33);
+            this.btnOpen.TabIndex = 7;
+            this.btnOpen.Text = "Open Image";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(613, 267);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(175, 33);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save Image";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 420);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(294, 249);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 33);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Open Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(12, 306);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(175, 69);
+            this.btnSubtract.TabIndex = 11;
+            this.btnSubtract.Text = "Subtract";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnWebcam
+            // 
+            this.btnWebcam.Location = new System.Drawing.Point(314, 450);
+            this.btnWebcam.Name = "btnWebcam";
+            this.btnWebcam.Size = new System.Drawing.Size(121, 33);
+            this.btnWebcam.TabIndex = 12;
+            this.btnWebcam.Text = "Use Webcam";
+            this.btnWebcam.UseVisualStyleBackColor = true;
+            this.btnWebcam.Click += new System.EventHandler(this.btnWebcam_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(314, 420);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(225, 24);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(441, 450);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(98, 33);
+            this.btnStop.TabIndex = 14;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 681);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnWebcam);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSepia);
+            this.Controls.Add(this.btnHistogram);
+            this.Controls.Add(this.btnInvert);
+            this.Controls.Add(this.btnGreyscale);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnGreyscale;
+        private System.Windows.Forms.Button btnInvert;
+        private System.Windows.Forms.Button btnHistogram;
+        private System.Windows.Forms.Button btnSepia;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnWebcam;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebCamLib;
+
+namespace ImageProcessing
+{
+    public partial class Form1 : Form
+    {
+        Bitmap loadImage, loadImage2, colorgreen, resultImage;
+        private Timer webcamTimer;
+        Boolean webcam = false;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.Size = pictureBox1.Size;
+
+            comboBox1.Items.Clear();
+            Device[] devices = DeviceManager.GetAllDevices();
+            comboBox1.Items.AddRange(devices);
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.SelectedIndex = 0;
+
+            webcamTimer = new Timer();
+            webcamTimer.Interval = 100;
+            webcamTimer.Tick += WebcamTimer_Tick;
+            webcamTimer.Start();
+        }
+
+        private void WebcamTimer_Tick(object sender, EventArgs e)
+        {
+            CaptureWebcamFrame();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if(openFile.ShowDialog() == DialogResult.OK)
+            {
+                loadImage = new Bitmap(openFile.FileName);
+                pictureBox1.Image = loadImage;
+                
+            }
+        }
+
+        private void btnGreyscale_Click(object sender, EventArgs e)
+        {
+            if (loadImage == null)
+            {
+                return;
+            }
+            resultImage = new Bitmap(loadImage.Width, loadImage.Height);
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            for(int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color bmpColor = bmp.GetPixel(i, j);
+                    int red = bmpColor.R;
+                    int green = bmpColor.G;
+                    int blue = bmpColor.B;
+                    int grey = (red + green + blue) / 3;
+                    resultImage.SetPixel(i, j, Color.FromArgb(grey, grey, grey));
+                }
+            }
+            pictureBox2.Image = resultImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+        }
+
+        private void btnInvert_Click(object sender, EventArgs e)
+        {
+            if (loadImage == null)
+            {
+                return;
+            }
+            resultImage = new Bitmap(loadImage.Width, loadImage.Height);
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color bmpColor = bmp.GetPixel(i, j);
+                    int red = bmpColor.R;
+                    int green = bmpColor.G;
+                    int blue = bmpColor.B;
+                    resultImage.SetPixel(i, j, Color.FromArgb(255-red, 255-green, 255-blue));
+                }
+            }
+            pictureBox2.Image = resultImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void btnHistogram_Click(object sender, EventArgs e)
+        {
+            if (loadImage == null)
+            {
+                return;
+            }
+            resultImage = new Bitmap(loadImage.Width, loadImage.Height);
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color bmpColor = bmp.GetPixel(i, j);
+                    int red = bmpColor.R;
+                    int green = bmpColor.G;
+                    int blue = bmpColor.B;
+                    int grey = (red + green + blue) / 3;
+                    resultImage.SetPixel(i, j, Color.FromArgb(grey, grey, grey));
+                }
+            }
+            Color sample;
+            int []histData = new int[256];
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    sample = resultImage.GetPixel(i, j);
+                    histData[sample.R]++;
+                }
+            }
+            Bitmap visualGraph = new Bitmap(256,800);
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < 800; j++)
+                {
+                    visualGraph.SetPixel(i, j, Color.White);
+                }
+            }
+
+            // datas
+
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < Math.Min(histData[i]/5,800); j++)
+                {
+                    visualGraph.SetPixel(i, 799 - j, Color.Black);
+                }
+            }
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Image = visualGraph;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            resultImage.Save(saveFileDialog1.FileName + ".png");
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            if(loadImage==null)
+            {
+                return;
+            }
+            if(loadImage2==null)
+            {
+                return;
+            }
+            int maxWidth = Math.Max(loadImage.Width, loadImage2.Width);
+            int maxHeight = Math.Max(loadImage.Height, loadImage2.Height);
+
+            loadImage = (Bitmap)loadImage.GetThumbnailImage(maxWidth, maxHeight, null, IntPtr.Zero);
+            loadImage2 = (Bitmap)loadImage2.GetThumbnailImage(maxWidth, maxHeight, null, IntPtr.Zero);
+
+            if (loadImage == null && loadImage2 == null)
+            {
+                return;
+            }
+            resultImage = loadImage2;
+            pictureBox2.Image = resultImage;
+
+            Color mygreen = Color.FromArgb(0, 0, 255);
+            int greygreen = (mygreen.R + mygreen.G + mygreen.B) / 3;
+            int threshold = 5;
+
+            for (int x = 0; x < loadImage2.Width; x++)
+            {
+                for(int y = 0; y < loadImage2.Height; y++)
+                {
+                    Color pixel = loadImage2.GetPixel(x, y);
+                    Color backpixel = loadImage.GetPixel(x, y);
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    int subtractvalue = Math.Abs(grey - greygreen);
+                    if (subtractvalue > threshold)
+                        resultImage.SetPixel(x, y, pixel);
+                    else
+                        resultImage.SetPixel(x, y, backpixel);
+                }
+            }
+            pictureBox2.Image = resultImage;
+        }
+
+        private void btnWebcam_Click(object sender, EventArgs e)
+        {
+            webcam = true;
+        }
+
+        private void CaptureWebcamFrame()
+        {
+            if(!webcam)
+            {
+                return;
+            }
+            Device[] devices = DeviceManager.GetAllDevices();
+
+            if (devices.Length > 0)
+            {
+                IDataObject data;
+                Image bmap;
+                Device d = DeviceManager.GetDevice(comboBox1.SelectedIndex);
+                d.Sendmessage();
+                data = Clipboard.GetDataObject();
+                bmap = (Image)(data.GetData("System.Drawing.Bitmap", true));
+
+
+                if (bmap != null)
+                {
+                    loadImage2 = new Bitmap(bmap);
+                    pictureBox3.Image = loadImage2;
+                }
+                else
+                {
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("No webcam found.");
+            }
+        }
+
+        private void btnSelectWebcam_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            webcam = false;
+        }
+
+        private void btnSepia_Click(object sender, EventArgs e)
+        {
+            if (loadImage == null)
+            {
+                return;
+            }
+            resultImage = new Bitmap(loadImage.Width, loadImage.Height);
+            Bitmap bmp = new Bitmap(pictureBox1.Image);
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color bmpColor = bmp.GetPixel(i, j);
+                    int red = bmpColor.R;
+                    int green = bmpColor.G;
+                    int blue = bmpColor.B;
+                    int grey = (red + green + blue) / 3;
+                    resultImage.SetPixel(i, j, Color.FromArgb(grey, (int)(grey * 0.95), (int)(grey * 0.82)));
+                }
+            }
+            pictureBox2.Image = resultImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                loadImage2 = new Bitmap(openFile.FileName);
+                pictureBox3.Image = loadImage2;
+
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if(loadImage==null)
+            {
+                return;
+            }
+            resultImage = new Bitmap(loadImage.Width,loadImage.Height);
+            for (int i = 0; i < loadImage.Width; i++)
+            {
+                for (int j = 0; j < loadImage.Height; j++)
+                {
+                    Color bmpColor = loadImage.GetPixel(i, j);
+                    resultImage.SetPixel(i, j, bmpColor);
+                }
+            }
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.Image = resultImage;
+            
+        }
+    }
+}
